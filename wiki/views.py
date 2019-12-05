a910bd8baa4418b45dc517e191328eca73263536
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-12-05 15:52:38 -0800

    tweaked settings for deployment

--- wiki/views.py
+++ wiki/views.py
@@ -16,7 +16,7 @@ class PageListView(ListView):
 
     def get(self, request):
         """ GET a list of Pages. """
-        pages = self.get_queryset().all().order_by('-date')
+        pages = self.get_queryset().all().order_by('-created')
         return render(request, 'list.html', {
           'pages': pages
         })
